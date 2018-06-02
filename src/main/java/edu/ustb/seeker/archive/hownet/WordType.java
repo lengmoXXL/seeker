2a6981c7af165a8fb5520cc55567976698803b84
owly <1075749478@qq.com>
2018-06-02 11:14:54 +0800

    git add hownet files.

---
+++ src/main/java/edu/ustb/seeker/archive/hownet/WordType.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2008 SKLSDE(TravelState Key Laboratory of Software Development and Environment, Beihang University)., All Rights Reserved.
+ */
+package edu.ustb.seeker.archive.hownet;
+
+public enum WordType {PREFIX,
+    PREP,
+    ECHO,
+    EXPR,
+    SUFFIX,
+    PUNC,
+    N,
+    ADV,
+    CLAS,
+    COOR,
+    CONJ,
+    V,
+    STRU,
+    PP,
+    P,
+    ADJ,
+    PRON,
+    AUX,
+    NUM;
+}
